517f4fce0cdddf1e4ca458f439a33f4a4660343a
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-03-02 23:31:07 +0600

    Successfully connected to MongoDB done

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require("express");
 require("dotenv").config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const PORT = process.env.PORT
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.sltbrlg.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 
 

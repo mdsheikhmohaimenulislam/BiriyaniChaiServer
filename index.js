eba4f565ebd54cc9950813734e038bed4eff3fb7
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-03-03 16:01:07 +0600

    cors middelwares create done

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT;
 
 // middlewares
-// app.use(cors());       // allow requests from frontend
+app.use(cors());       // allow requests from frontend
 app.use(express.json()); // parse JSON bodies
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.sltbrlg.mongodb.net/?appName=Cluster0`;
